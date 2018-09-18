f6173d929ecacac0c324c2d060109052d59d109a
disasteroftheuniverse <patrick@patrickpineda.net>
2018-09-18 12:57:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ceros-bodymovin
+attempting to add bodymovin animated SVGs to the Ceros platform
---
+++ main.js
@@ -0,0 +1,7 @@
+(function(){
+
+    console.log("Hello World");
+
+});
+
+//just testing to see if I can get CEROS to load in an external JS file.
